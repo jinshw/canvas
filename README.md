9b0e64f9cb19cd66d43bd1c29e75857352b38e85
jinshw <jinshengwang2007@163.com>
2018-06-10 22:28:15 +0800

    init

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 * 画板
     drawboard.html
 
+* base
+    index.html ---- 图片放大缩小、添加水印
+    offcanvas.html ---- 实现放大镜
+    data.html ---- 图片像素级控制，实现：灰度、黑白、反色、模糊、马赛克等滤镜（需要在http服务器下运行）
+    createimagedata.html ---- 创建空的imagedata （需要在http服务器下运行）
+
+
+* filter 
+    blur-pc.html ---- 电脑端的滤镜
---
+++ filter/blur.js
@@ -0,0 +1,87 @@
+var canvasHeight = window.innerHeight
+var canvasWidht = window.innerWidth
+
+var canvas = document.getElementById("canvas")
+var context = canvas.getContext("2d")
+canvas.width = canvasWidht
+canvas.height = canvasHeight
+
+var img = new Image()
+var radius = 50
+var clippingRegion = {x: -1, y: -1, r: radius}
+img.src = "1.jpg"
+img.onload = function (e) {
+    initCanvas()
+}
+function initCanvas() {
+    clippingRegion = {
+        x: Math.random() * (canvas.width - 2 * radius) + radius,
+        y: Math.random() * (canvas.height - 2 * radius) + radius,
+        r: radius
+    }
+    draw(img, clippingRegion)
+}
+function setClippingRegion() {
+    context.beginPath()
+    context.arc(clippingRegion.x, clippingRegion.y, clippingRegion.r, 0, Math.PI * 2, false)
+    context.clip()
+
+}
+
+function draw(img, clippingRegion) {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.save()
+    setClippingRegion(clippingRegion)
+    context.drawImage(img, 0, 0, canvas.width, canvas.height)
+    context.restore()
+}
+
+function show() {
+    var theAnimation = setInterval(function () {
+        console.log("theAnimation..")
+        clippingRegion.r += 20
+        if (clippingRegion.r > 2 * Math.max(canvas.width, canvas.height)) {
+            clearInterval(theAnimation)
+        }
+        draw(img, clippingRegion)
+
+    }, 30)
+
+}
+
+function reset() {
+    // initCanvas()
+    tempR = 2 * Math.min(canvas.width, canvas.height)
+
+    // clippingRegion = {
+    //     x: Math.random() * (canvas.width - 2 * radius) + radius,
+    //     y: Math.random() * (canvas.height - 2 * radius) + radius,
+    //     r: tempR
+    // }
+    clippingRegion = {
+        x: Math.random() * (canvas.width - 2 * radius) + radius,
+        y: Math.random() * (canvas.height - 2 * radius) + radius,
+    }
+    var animation = setInterval(
+        function () {
+
+            context.clearRect(0, 0, canvas.width, canvas.height)
+            context.save()
+            // setClippingRegion(clippingRegion)
+            context.beginPath()
+            tempR -= 30
+            if(tempR<radius){
+                clearInterval(animation)
+                tempR = radius
+                clippingRegion.r = radius
+            }
+
+            context.arc(clippingRegion.x, clippingRegion.y, tempR, 0, Math.PI * 2, false)
+            context.clip()
+            context.drawImage(img, 0, 0, canvas.width, canvas.height)
+            context.restore()
+        }, 30
+    )
+
+
+}
